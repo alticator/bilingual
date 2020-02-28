6d7fd517ad439d67ca9fad173f2f880ef1c21b84
alticator <main@alticator.com>
2020-02-28 19:37:22 +0200

    Version 3
    1. Added new game: Verbs
    2. Updates to the game menu.
    3. Added buttons for special characters.
    4. Added animations.
    5. Other changes

--- database.js
+++ database.js
@@ -8,5 +8,10 @@ var database = {
         english: ["the house", "the car", "the room", "the table", "the floor", "the roof", "the wall", "the kitchen", "the window"],
         englishSecondary: ["the home", "the car", "the room", "the table", "the floor", "the roof", "the wall", "the kitchen", "the window"],
         french: ["la maison", "la voiture", "la chambre", "la table", "le sol", "le toit", "le mur", "la cuisine", "la fenêtre"]
+    },
+    verbs: {
+        english: ["to walk", "to run", "I walk", "you walk (informal)", "you walk (formal)", "he walks", "she walks", "we walk", "you walk (plural)", "they walk (masculine)", "they walk (feminine)", "I run", "you run (informal)", "you run (formal)", "he runs", "she runs", "we run", "you run (plural)", "they run (masculine)", "they run (feminine)"],
+        englishSecondary: ["walk", "run", "I walk", "you walk", "you walk", "he walks", "she walks", "we walk", "you walk", "they walk", "they walk", "I run", "you run", "you run", "he runs", "she runs", "we run", "you run", "they run", "they run"],
+        french: ["marcher", "courir", "je marche", "tu marches", "vous marchez", "il marche", "elle marche", "nous marchons", "vous marchez", "ils marchent", "elles marchent", "je cours", "tu cours", "vous courez", "il court", "elle court", "nous courrons", "vous courez", "ils courent", "elles courent"]
     }
 };
--- scripts.js
+++ scripts.js
@@ -7,20 +7,25 @@ function getInput() {
 }
 
 var gameType = getData("game-type");
-new rect(0, 0, 100, 100, "#00D0FF");
 var gameLoop = setInterval(game, 20);
 var questionType = 1;
 var goal = new rect(90, 40, 10, 20, "red");
 var timer = new rect(0, 40, 20, 20, "blue");
-var timerText = new textObj("Timer", 5, 60, "20vh Arial", "white", "left");
+var timerText = new textObj("Timer", 5, 60, "20vh Arial", "#00D0FF", "left");
 var wordId = random(0, database.pronouns.english.length);
-var wordContainer = new rect(40, -10, 20, 10, "#00D0FF");
-var word = new textObj("Loading...", 50, -10, "24px Arial", "white", "center");
+var wordContainer = new rect(40, -10, 20, 5, "#00D0FF");
+var word = new textObj("Loading...", 50, -10, "3vh Arial", "white", "center");
 updateWord();
 document.addEventListener("keydown", keyPress);
 var score = 0;
 var scoreBoard = new textObj("Score: " + score, 10, 20, "24px Arial", "white", "left");
-word.Yv = 0.25;
+speed = 0.25;
+word.Yv = speed;
+timePaused = false;
+
+function add(character) {
+    document.getElementById("input").value += character;
+}
 
 function keyPress(event) {
     if (event.key == "Enter") {
@@ -30,10 +35,52 @@ function keyPress(event) {
 
 function correct() {
     score++;
+    document.getElementById("input").value = "";
+    timePaused = true;
+    word.Yv = 0;
+    word.Xv = 5;
+    wordContainer.Xv = 5;
+    setTimeout(correctTwo, 1000);
+}
+
+function correctTwo() {
+    word.Yv = speed;
+    word.Xv = 0;
+    wordContainer.Xv = 0;
+    word.x = 50;
+    word.y = 0;
+    wordContainer.x = 40;
     timer.width -= 5;
     updateWord();
-    word.y = 0;
+    timePaused = false;
+}
+
+function incorrect() {
+    score++;
     document.getElementById("input").value = "";
+    timePaused = true;
+    word.string = "Incorrect";
+    word.color = "red";
+    word.Yv = 0;
+    setTimeout(incorrectTwo, 1000);
+}
+
+function incorrectTwo() {
+    word.Xv = -5;
+    wordContainer.Xv = -5;
+    setTimeout(incorrectThree, 1000);
+}
+
+function incorrectThree() {
+    word.color = "white";
+    word.Yv = speed;
+    word.Xv = 0;
+    wordContainer.Xv = 0;
+    word.x = 50;
+    word.y = 0;
+    wordContainer.x = 40;
+    updateWord();
+    timePaused = false;
 }
 
 function checkAnswer() {
@@ -42,10 +89,16 @@ function checkAnswer() {
             if (getInput() == database.pronouns.french[wordId]) {
                 correct();
             }
+            else {
+                incorrect();
+            }
         }
         if (questionType == 2) {
             if (getInput() == database.pronouns.englishSecondary[wordId] || getInput() == database.pronouns.english[wordId]) {
                 correct();
+            }
+            else {
+                incorrect();
             }
         }
     }
@@ -54,10 +107,34 @@ function checkAnswer() {
             if (getInput() == database.living.french[wordId]) {
                 correct();
             }
+            else {
+                incorrect();
+            }
         }
         if (questionType == 2) {
             if (getInput() == database.living.englishSecondary[wordId] || getInput() == database.living.english[wordId]) {
                 correct();
+            }
+            else {
+                incorrect();
+            }
+        }
+    }
+    else if (gameType == "verbs") {
+        if (questionType == 1) {
+            if (getInput() == database.verbs.french[wordId]) {
+                correct();
+            }
+            else {
+                incorrect();
+            }
+        }
+        if (questionType == 2) {
+            if (getInput() == database.verbs.englishSecondary[wordId] || getInput() == database.verbs.english[wordId]) {
+                correct();
+            }
+            else {
+                incorrect();
             }
         }
     }
@@ -85,25 +162,41 @@ function updateWord() {
             word.string = database.living.french[wordId];
         }
     }
+    else if (gameType == "verbs") {
+        if (questionType == 1) {
+            wordId = random(0, database.verbs.english.length);
+            word.string = database.verbs.english[wordId];
+        }
+        else if (questionType == 2) {
+            wordId = random(0, database.verbs.french.length);
+            word.string = database.verbs.french[wordId];
+        }
+    }
 }
 
 function gameOver() {
     clearInterval(gameLoop);
     clearObjects();
     new textObj("Game Over", 50, 50, "36px Arial", "#00D0FF", "center");
+    var panel = document.getElementById("container");
+    panel.parentNode.removeChild(panel);
 }
 
 function game() {
     scoreBoard.string = "Score: " + score;
     wordContainer.x = word.x - 10;
-    wordContainer.y = word.y - 5;
+    wordContainer.y = word.y - 3.5;
+    document.getElementById("input").focus();
     if (word.y > 100) {
+        document.getElementById("input").value = "";
         updateWord();
         word.y = 0;
     }
     if (objectCollision(timer, goal)) {
         gameOver();
     }
-    timer.width += 0.05;
+    if (timePaused == false) {
+        timer.width += 0.04;
+    }
     updateAll();
 }
